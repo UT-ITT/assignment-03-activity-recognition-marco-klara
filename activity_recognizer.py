a5c18dddb67e8462e143e2ead20364451f31b124
Marco Schneider <mydiversity-48048@gmx.de>
2026-05-13 16:59:50 +0200

    added feature extraction function

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -4,8 +4,10 @@ import pandas as pd
 from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
 
 
+THIS_DIR = Path(__file__).resolve().parent
+DATA_DIR = THIS_DIR.parent / "assignment-03-training-data-join-this-team-to-upload-your-data"
 
-DATA_DIR = Path("/home/klara/Dokumente/Uni/ITT/woche_4/assignment-03-training-data-join-this-team-to-upload-your-data")
+#DATA_DIR = Path("/home/klara/Dokumente/Uni/ITT/woche_4/assignment-03-training-data-join-this-team-to-upload-your-data")
 
 def modify_data(df):
     df = df.dropna()
@@ -40,8 +42,10 @@ def load_all_csv_data(data_dir: Path = DATA_DIR) -> pd.DataFrame:
         activity = parts[1] if len(parts) >= 3 else None
 
         df = pd.read_csv(csv_file)
+
         if "id" in df.columns and "id.1" in df.columns:
             df = df.drop(columns=["id.1"])
+
         df = df.assign(
             activity=activity,
         )
@@ -54,12 +58,84 @@ def load_all_csv_data(data_dir: Path = DATA_DIR) -> pd.DataFrame:
 
     return df
 
+def csv_feature_extraction(data_directory):
 
+    folder = data_directory
 
-data = load_all_csv_data()
-modified_data = modify_data(data)
-print(f"Loaded {len(data)} rows")
-print(modified_data)
+    # collect all csv files inside the directory
+    csv_files = list(folder.rglob("*.csv"))
+
+    all_features = []
+
+    # for every file, extract its features (statistical measurements of its column values) 
+    # and create a new row in the feature Dataframe
+    for file in csv_files:
+
+        df = pd.read_csv(file)
+
+        # drop rows that contain NaN / are empty
+        df = df.dropna()
+
+        # only allow certain columns for feature extraction
+        valid_columns = ("acc_x", "acc_y", "acc_z","gyro_x", "gyro_y", "gyro_z")
+
+        # skip files that dont contain all required columns (valid_columns) or contain zero columns
+        skip = False
+        for col in valid_columns:
+            if col in df.columns:
+                if (df[col] == 0).all():
+                    skip = True
+                    break
+            else:
+                skip = True
+                break
+        if skip:
+            continue
+
+        # Get activity from filename
+        filename_without_suffix = file.stem
+        parts = filename_without_suffix.split("-")
+
+        # print error if the file doesnt have the right format i.e. name-activity-number.csv
+        if len(parts) > 3:
+            print(f"wrong fileformat: {file.name}")
+            continue
+
+        activity = parts[1]
+
+        features = {
+            "activity": activity,
+        }
+        
+        for col in df.columns:
+            if (col in valid_columns):
+                column = df[col]
+
+                features[f"{col}_mean"] = column.mean()
+                features[f"{col}_std"] = column.std()
+                features[f"{col}_min"] = column.min()
+                features[f"{col}_max"] = column.max()
+                features[f"{col}_median"] = column.median()
+                features[f"{col}_q25"] = column.quantile(0.25)
+                features[f"{col}_q75"] = column.quantile(0.75)
+                features[f"{col}_iqr"] = (column.quantile(0.75) - column.quantile(0.25))
+                features[f"{col}_skew"] = column.skew()
+
+        all_features.append(features)
+    
+    feature_df = pd.DataFrame(all_features)
+
+    return feature_df
+
+data = csv_feature_extraction(DATA_DIR)
+data.to_csv("debug.csv")
+print(data.columns)
+print(data.head(5))
+
+#data = load_all_csv_data()
+#modified_data = modify_data(data)
+#print(f"Loaded {len(data)} rows")
+#print(modified_data)
 
 
 
